cdd1097cf3bc2f50a548e482b0b6ccb450ec2d5c
cmrivera8 <carlos.rivera8528@hotmail.com>
2023-02-08 07:42:47 +0100

    Keep most important variables and add author

--- settings.sh
+++ settings.sh
@@ -1,15 +1,17 @@
-# Defining board's name
-# Options: BeagleBoard_X15
-export BOARD_NAME="BeagleBoard_X15"
-
-# Defining Buildroot location (Absolute path)
-export BR_DIR=/home/cmrivera/Project_M2/buildroot_BeagleBoard_X15
-
+# Author: Carlos RIVERA
 # Defining target board's IP
 export IP=192.168.137.8
 
-# Defining NFS location in host computer (to install executables and modules)
-export NFS=/home/cmrivera/Project_M2/nfs
+#Source environment-setup of Processor SDK
+export PSDK_LINUX=/home/cmrivera/ti-processor-sdk-linux-rt-am57xx-evm-06.03.00.106
+source ${PSDK_LINUX}/linux-devkit/environment-setup
 
-# Create necessary variables to compile projects (app directory)
-export APP=export APP=$PWD/app
+#Variables creation for OpenCL compilation
+export HOST_ROOTDIR=${PSDK_LINUX}/linux-devkit/sysroots/x86_64-arago-linux
+export TARGET_ROOTDIR=${PSDK_LINUX}/linux-devkit/sysroots/armv7at2hf-neon-linux-gnueabi
+
+export CLOCL=${HOST_ROOTDIR}/usr/bin/clocl
+
+export TI_OCL_CGT_INSTALL=${HOST_ROOTDIR}/usr/share/ti/cgt-c6x
+export TI_OCL_INSTALL=${TARGET_ROOTDIR}
+export CL6X=${TI_OCL_CGT_INSTALL}/bin/cl6x
